0fa18e0c046fc660e15eaccaac45214123cb6117
jack12202 <zjk12202@gmail.com>
2026-06-15 23:35:01 +0800

    Switch recharge links and pricing

--- api/recharge-go.js
+++ api/recharge-go.js
@@ -1,14 +1,6 @@
-const { CHANNELS, getActiveChannel } = require('../lib/channels');
+const { CHANNELS } = require('../lib/channels');
 
 module.exports = async function handler(req, res) {
-  try {
-    const active = await getActiveChannel();
-
-    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0');
-    res.redirect(302, CHANNELS[active].url);
-  } catch (error) {
-    console.error(error);
-    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0');
-    res.redirect(302, CHANNELS.ow800.url);
-  }
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0');
+  res.redirect(302, CHANNELS.czgpt.url);
 };
