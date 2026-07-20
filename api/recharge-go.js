86b3ca531c5779aee517093ad9b663009c54bca9
jack12202 <zjk12202@gmail.com>
2026-07-20 17:14:23 +0800

    Route activation through AIPass

--- api/recharge-go.js
+++ api/recharge-go.js
@@ -1,6 +1,6 @@
-const { CHANNELS } = require('../lib/channels');
+const CENTRAL_ACTIVATION_URL = 'https://www.aipass.me/api/recharge-go';
 
 module.exports = async function handler(req, res) {
   res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0');
-  res.redirect(302, CHANNELS.czgpt.url);
+  res.redirect(302, CENTRAL_ACTIVATION_URL);
 };
